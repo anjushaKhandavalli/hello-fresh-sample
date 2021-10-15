3a10986b91629f651385d1da6e288b3513586329
anjushaKhandavalli <anjusha.khandavalli@gmail.com>
2021-10-16 00:00:37 +0530

    Update fooditem image urls to be unique

--- src/mockData/Receipes.js
+++ src/mockData/Receipes.js
@@ -1,7 +1,7 @@
 export const foodItems = [
     {
         "id": "94298",
-        "imageUrl": "https://source.unsplash.com/random/1600x900/?food",
+        "imageUrl": "https://cdn.pixabay.com/photo/2021/10/10/18/04/food-6697924_1280.jpg",
         "name": "3 Chicken Wings",
         "description": "Tender, Spicy and Juicy.",
         "recipeData": {
@@ -24,7 +24,7 @@ export const foodItems = [
     },
     {
         "id": "94301",
-        "imageUrl": "https://source.unsplash.com/random/1600x900/?food",
+        "imageUrl": "https://cdn.pixabay.com/photo/2021/10/10/18/03/food-6697917_1280.jpg",
         "name": "Chicken Livers and Portuguese Roll",
         "description": "Chicken Livers Topped with PERi-PERi Sauce",
         "recipeData": {
@@ -47,7 +47,7 @@ export const foodItems = [
     },
     {
         "id": "94304",
-        "imageUrl": "https://source.unsplash.com/random/1600x900/?food",
+        "imageUrl": "https://cdn.pixabay.com/photo/2021/10/10/18/10/rice-6697956_1280.jpg",
         "name": "Spicy Mixed Olives (V)",
         "description": "Co-Starring Garlic, Pepper",
         "recipeData": {
@@ -70,7 +70,7 @@ export const foodItems = [
     },
     {
         "id": "94307",
-        "imageUrl": "https://source.unsplash.com/random/1600x900/?food",
+        "imageUrl": "https://cdn.pixabay.com/photo/2021/10/10/18/11/chicken-kabob-6697959_1280.jpg",
         "name": "Hummus with PERI-PERI Drizzle (V)",
         "description": "Pour Smoky PERi-PERi Oil Over Creamy Hummus",
         "recipeData": {
@@ -93,7 +93,7 @@ export const foodItems = [
     },
     {
         "id": "94310",
-        "imageUrl": "https://source.unsplash.com/random/1600x900/?food",
+        "imageUrl": "https://cdn.pixabay.com/photo/2021/10/10/12/59/drink-6696596_1280.jpg",
         "name": "Red Pepper Dip (V)",
         "description": "Dive Into Temping Roasted Red Pepper",
         "recipeData": {
@@ -116,7 +116,7 @@ export const foodItems = [
     },
     {
         "id": "94313",
-        "imageUrl": "https://source.unsplash.com/random/1600x900/?food",
+        "imageUrl": "https://cdn.pixabay.com/photo/2017/09/16/19/21/salad-2756467_1280.jpg",
         "name": "Appeteaser Platter",
         "description": "Choose any Three Appeteasers",
         "recipeData": {
@@ -139,7 +139,7 @@ export const foodItems = [
     },
     {
         "id": "94316",
-        "imageUrl": "https://source.unsplash.com/random/1600x900/?food",
+        "imageUrl": "https://cdn.pixabay.com/photo/2021/10/10/18/10/chicken-kabob-6697957_1280.jpg",
         "name": "All Together Now (V)",
         "description": "Spicy Mixed Olives, Red Pepper Dip.",
         "recipeData": {
@@ -162,7 +162,7 @@ export const foodItems = [
     },
     {
         "id": "94334",
-        "imageUrl": "https://source.unsplash.com/random/1600x900/?food",
+        "imageUrl": "https://cdn.pixabay.com/photo/2015/03/26/09/39/cupcakes-690040_1280.jpg",
         "name": "PERi-PERi Wedges (P)",
         "description": "Spicy Mixed Olives, Red Pepper Dip.",
         "recipeData": {
@@ -185,7 +185,7 @@ export const foodItems = [
     },
     {
         "id": "94337",
-        "imageUrl": "https://source.unsplash.com/random/1600x900/?food",
+        "imageUrl": "https://cdn.pixabay.com/photo/2015/07/31/12/07/soup-greens-869075_1280.jpg",
         "name": "Corn-On-The-Cob",
         "description": "Spicy Mixed Olives, Red Pepper Dip.",
         "recipeData": {
@@ -208,7 +208,7 @@ export const foodItems = [
     },
     {
         "id": "94349",
-        "imageUrl": "https://source.unsplash.com/random/1600x900/?food",
+        "imageUrl": "https://cdn.pixabay.com/photo/2021/10/12/22/14/tree-6704907_1280.jpg",
         "name": "1/4 Chicken",
         "description": "Quarter Chicken Marinated with PERi-PERi Sauce",
         "recipeData": {
@@ -231,7 +231,7 @@ export const foodItems = [
     },
     {
         "id": "94352",
-        "imageUrl": "https://source.unsplash.com/random/1600x900/?food",
+        "imageUrl": "https://cdn.pixabay.com/photo/2021/10/12/08/33/food-6702875_1280.jpg",
         "name": "1/2 Chicken",
         "description": "Half Chicken Marinated with PERi-PERi Sauce",
         "recipeData": {
@@ -254,7 +254,7 @@ export const foodItems = [
     },
     {
         "id": "94355",
-        "imageUrl": "https://source.unsplash.com/random/1600x900/?food",
+        "imageUrl": "https://cdn.pixabay.com/photo/2015/05/04/10/16/vegetables-752153_1280.jpg",
         "name": "5 Chicken Wings",
         "description": "Original Flavours or PERi-PEREi Crusted (P)",
         "recipeData": {
@@ -277,7 +277,7 @@ export const foodItems = [
     },
     {
         "id": "94358",
-        "imageUrl": "https://source.unsplash.com/random/1600x900/?food",
+        "imageUrl": "https://cdn.pixabay.com/photo/2021/10/08/16/40/food-6691849_1280.jpg",
         "name": "Wing Roulette",
         "description": "10 Wings in PERi-PERi Flavors",
         "recipeData": {
@@ -300,7 +300,7 @@ export const foodItems = [
     },
     {
         "id": "94361",
-        "imageUrl": "https://source.unsplash.com/random/1600x900/?food",
+        "imageUrl": "https://cdn.pixabay.com/photo/2021/10/13/17/44/bread-6707265_1280.jpg",
         "name": "Whole Chicken",
         "description": "Whole Chicken Marinated in PERi-PERi Sauce.",
         "recipeData": {
@@ -323,7 +323,7 @@ export const foodItems = [
     },
     {
         "id": "94364",
-        "imageUrl": "https://source.unsplash.com/random/1600x900/?food",
+        "imageUrl": "https://cdn.pixabay.com/photo/2021/10/10/18/12/rice-6697969_1280.jpg",
         "name": "Chicken Butterfly",
         "description": "Succulent Double Chicken Breast in Crispy Skin",
         "recipeData": {
@@ -346,7 +346,7 @@ export const foodItems = [
     },
     {
         "id": "94367",
-        "imageUrl": "https://source.unsplash.com/random/1600x900/?food",
+        "imageUrl": "https://cdn.pixabay.com/photo/2016/02/19/11/30/shrimp-1209744_1280.jpg",
         "name": "Espetada",
         "description": "Tender Marinated Chicken Thighs and Fresh Peppers",
         "recipeData": {
@@ -369,7 +369,7 @@ export const foodItems = [
     },
     {
         "id": "94370",
-        "imageUrl": "https://source.unsplash.com/random/1600x900/?food",
+        "imageUrl": "https://cdn.pixabay.com/photo/2021/10/10/18/07/meal-6697944_1280.jpg",
         "name": "Espetada Carnival",
         "description": "Flame-Grilled PERi-PERi Thighs, Stuffed with Feta",
         "recipeData": {
@@ -392,7 +392,7 @@ export const foodItems = [
     },
     {
         "id": "94373",
-        "imageUrl": "https://source.unsplash.com/random/1600x900/?food",
+        "imageUrl": "https://cdn.pixabay.com/photo/2014/11/05/15/57/salmon-518032_1280.jpg",
         "name": "Grilled Chicken Tenders",
         "description": "Juicy and Tender, Delicious with All Sides",
         "recipeData": {
@@ -415,7 +415,7 @@ export const foodItems = [
     },
     {
         "id": "94379",
-        "imageUrl": "https://source.unsplash.com/random/1600x900/?food",
+        "imageUrl": "https://cdn.pixabay.com/photo/2017/03/23/19/57/asparagus-2169305_1280.jpg",
         "name": "Full Platter",
         "description": "For 3-4 People, Whole Chicken with  2 Large",
         "recipeData": {
@@ -438,7 +438,7 @@ export const foodItems = [
     },
     {
         "id": "94382",
-        "imageUrl": "https://source.unsplash.com/random/1600x900/?food",
+        "imageUrl": "https://cdn.pixabay.com/photo/2016/01/22/02/13/meat-1155132_1280.jpg",
         "name": "No Bones Platter",
         "description": "For 2-3 People, Chicken Butterfly and a Espetada",
         "recipeData": {
@@ -461,7 +461,7 @@ export const foodItems = [
     },
     {
         "id": "94385",
-        "imageUrl": "https://source.unsplash.com/random/1600x900/?food",
+        "imageUrl": "https://cdn.pixabay.com/photo/2014/05/05/19/52/charcuterie-338498_1280.jpg",
         "name": "Jumbo Platter",
         "description": "For 4-6 People, 2 Whole Chickens and 5 Large Sides",
         "recipeData": {
@@ -484,7 +484,7 @@ export const foodItems = [
     },
     {
         "id": "94319",
-        "imageUrl": "https://source.unsplash.com/random/1600x900/?food",
+        "imageUrl": "https://cdn.pixabay.com/photo/2016/04/04/17/22/meal-1307604_1280.jpg",
         "name": "PERi-PERi Chips (P)",
         "description": "Crunchy Chips with PERi-PERi Sauce",
         "recipeData": {
@@ -507,7 +507,7 @@ export const foodItems = [
     },
     {
         "id": "94322",
-        "imageUrl": "https://source.unsplash.com/random/1600x900/?food",
+        "imageUrl": "https://cdn.pixabay.com/photo/2015/04/08/13/13/food-712665_1280.jpg",
         "name": "Spicy Rice",
         "description": "Spicy fried rice",
         "recipeData": {
@@ -530,7 +530,7 @@ export const foodItems = [
     },
     {
         "id": "94325",
-        "imageUrl": "https://source.unsplash.com/random/1600x900/?food",
+        "imageUrl": "https://cdn.pixabay.com/photo/2014/06/11/17/00/food-366875_1280.jpg",
         "name": "Coleslaw (M)",
         "description": "Cabbage, Carrot, Nando&amp;#039;s Special Mayonnaise",
         "recipeData": {
@@ -553,7 +553,7 @@ export const foodItems = [
     },
     {
         "id": "94328",
-        "imageUrl": "https://source.unsplash.com/random/1600x900/?food",
+        "imageUrl": "https://cdn.pixabay.com/photo/2016/11/06/23/31/breakfast-1804457_1280.jpg",
         "name": "Grilled Veg",
         "description": "Grilled Assorted Vegetables",
         "recipeData": {
@@ -576,7 +576,7 @@ export const foodItems = [
     },
     {
         "id": "94331",
-        "imageUrl": "https://source.unsplash.com/random/1600x900/?food",
+        "imageUrl": "https://cdn.pixabay.com/photo/2017/05/07/08/56/pancakes-2291908_1280.jpg",
         "name": "Garlic Bread",
         "description": "Toasted Garlic Flavored Bread",
         "recipeData": {
@@ -599,7 +599,7 @@ export const foodItems = [
     },
     {
         "id": "94340",
-        "imageUrl": "https://source.unsplash.com/random/1600x900/?food",
+        "imageUrl": "https://cdn.pixabay.com/photo/2017/01/30/13/49/pancakes-2020863_1280.jpg",
         "name": "Portuguese Salad",
         "description": "Fresh Tomato, Nando&amp;#039;s Peri-Olives, Mixed Peppers",
         "recipeData": {
@@ -622,7 +622,7 @@ export const foodItems = [
     },
     {
         "id": "94343",
-        "imageUrl": "https://source.unsplash.com/random/1600x900/?food",
+        "imageUrl": "https://cdn.pixabay.com/photo/2016/11/29/11/15/fruits-1869132_1280.jpg",
         "name": "Mediterranean Salad",
         "description": "Piccolo Tomato, Cucumber, Sweet Peppers and Mixed Leaves.",
         "recipeData": {
@@ -645,7 +645,7 @@ export const foodItems = [
     },
     {
         "id": "94346",
-        "imageUrl": "https://source.unsplash.com/random/1600x900/?food",
+        "imageUrl": "https://cdn.pixabay.com/photo/2015/03/26/09/42/breakfast-690128_1280.jpg",
         "name": "Caesar Salad (M)",
         "description": "Cos Lettuce Sprinkled with Parmesan Cheese, Caesar",
         "recipeData": {
@@ -668,7 +668,7 @@ export const foodItems = [
     },
     {
         "id": "94388",
-        "imageUrl": "https://source.unsplash.com/random/1600x900/?food",
+        "imageUrl": "https://cdn.pixabay.com/photo/2016/03/09/15/30/breakfast-1246686_1280.jpg",
         "name": "Chicken Wrap (Y)",
         "description": "A Lightly Toasted Wrap, Generously Filled With Tender Chicken",
         "recipeData": {
@@ -691,7 +691,7 @@ export const foodItems = [
     },
     {
         "id": "94391",
-        "imageUrl": "https://source.unsplash.com/random/1600x900/?food",
+        "imageUrl": "https://cdn.pixabay.com/photo/2015/08/20/20/07/cereal-898073_1280.jpg",
         "name": "Chicken Breast Burger (M)",
         "description": "Served on A Toasted Portuguese Roll with Fresh Rocket, Tomato",
         "recipeData": {
@@ -714,7 +714,7 @@ export const foodItems = [
     },
     {
         "id": "94394",
-        "imageUrl": "https://source.unsplash.com/random/1600x900/?food",
+        "imageUrl": "https://cdn.pixabay.com/photo/2016/11/18/14/39/beans-1834984_1280.jpg",
         "name": "Chicken Caesar Wrap (M)",
         "description": "Served in A Toasted Wrap Full Of PERi-PERi Chicken",
         "recipeData": {
@@ -737,7 +737,7 @@ export const foodItems = [
     },
     {
         "id": "94397",
-        "imageUrl": "https://source.unsplash.com/random/1600x900/?food",
+        "imageUrl": "https://cdn.pixabay.com/photo/2015/12/08/00/26/food-1081707_1280.jpg",
         "name": "Chicken Pita (M)",
         "description": "Served in A Lightly Toasted Pita, Stuffed With Chicken Tenders,",
         "recipeData": {
@@ -760,7 +760,7 @@ export const foodItems = [
     },
     {
         "id": "12345",
-        "imageUrl": "https://source.unsplash.com/random/1600x900/?food",
+        "imageUrl": "https://cdn.pixabay.com/photo/2016/11/06/23/16/breakfast-1804436_1280.jpg/",
         "name": "Cataplana Algarve",
         "description": "PERi-PERi Chicken Thighs, Mixed Peppers, Chickpeas.",
         "recipeData": {
@@ -783,7 +783,7 @@ export const foodItems = [
     },
     {
         "id": "94316",
-        "imageUrl": "https://source.unsplash.com/random/1600x900/?food",
+        "imageUrl": "https://cdn.pixabay.com/photo/2021/01/16/09/05/meal-5921491_1280.jpg",
         "name": "All Together Now (V)",
         "description": "Spicy Mixed Olives, Red Pepper Dip, Hummus with PERi-PERi Drizzle.",
         "recipeData": {
@@ -806,7 +806,7 @@ export const foodItems = [
     },
     {
         "id": "94349",
-        "imageUrl": "https://source.unsplash.com/random/1600x900/?food",
+        "imageUrl": "https://cdn.pixabay.com/photo/2016/02/19/10/19/dish-1209170_1280.jpg",
         "name": "1/4 Chicken",
         "description": "Quarter Chicken Marinated with PERi-PERi Sauce and Grilled",
         "recipeData": {
@@ -829,7 +829,7 @@ export const foodItems = [
     },
     {
         "id": "94370",
-        "imageUrl": "https://source.unsplash.com/random/1600x900/?food",
+        "imageUrl": "https://cdn.pixabay.com/photo/2016/03/09/15/30/dessert-1246687_1280.jpg",
         "name": "Espetada Carnival",
         "description": "Flame-Grilled PERi-PERi Thighs, Stuffed with Feta and Parsley.",
         "recipeData": {
@@ -852,7 +852,7 @@ export const foodItems = [
     },
     {
         "id": "94325",
-        "imageUrl": "https://source.unsplash.com/random/1600x900/?food",
+        "imageUrl": "https://cdn.pixabay.com/photo/2021/02/08/12/40/lasagna-5994612_1280.jpg",
         "name": "Coleslaw (M)",
         "description": "Cabbage, Carrot, Nando&amp;#039;s Special Mayonnaise",
         "recipeData": {
@@ -875,7 +875,7 @@ export const foodItems = [
     },
     {
         "id": "94307",
-        "imageUrl": "https://source.unsplash.com/random/1600x900/?food",
+        "imageUrl": "https://cdn.pixabay.com/photo/2020/05/28/18/51/eat-5232255_1280.jpg",
         "name": "Hummus with PERI-PERI Drizzle (V)",
         "description": "Pour Smoky PERi-PERi Oil Over Creamy Hummus",
         "recipeData": {
@@ -898,7 +898,7 @@ export const foodItems = [
     },
     {
         "id": "94310",
-        "imageUrl": "https://source.unsplash.com/random/1600x900/?food",
+        "imageUrl": "https://cdn.pixabay.com/photo/2016/08/18/18/40/salad-1603608_1280.jpg",
         "name": "Red Pepper Dip (V)",
         "description": "Dive Into Temping Roasted Red Pepper",
         "recipeData": {
@@ -921,7 +921,7 @@ export const foodItems = [
     },
     {
         "id": "94313",
-        "imageUrl": "https://source.unsplash.com/random/1600x900/?food",
+        "imageUrl": "https://cdn.pixabay.com/photo/2014/04/05/11/19/celebration-315079_1280.jpg",
         "name": "Appeteaser Platter",
         "description": "Choose any Three Appeteasers",
         "recipeData": {
@@ -944,7 +944,7 @@ export const foodItems = [
     },
     {
         "id": "94316",
-        "imageUrl": "https://source.unsplash.com/random/1600x900/?food",
+        "imageUrl": "https://cdn.pixabay.com/photo/2016/10/23/09/37/fried-rice-1762493_1280.jpg",
         "name": "All Together Now (V)",
         "description": "Spicy Mixed Olives, Red Pepper Dip.",
         "recipeData": {
@@ -967,7 +967,7 @@ export const foodItems = [
     },
     {
         "id": "94334",
-        "imageUrl": "https://source.unsplash.com/random/1600x900/?food",
+        "imageUrl": "https://cdn.pixabay.com/photo/2015/09/02/12/43/meal-918638_1280.jpg",
         "name": "PERi-PERi Wedges (P)",
         "description": "Spicy Mixed Olives, Red Pepper Dip.",
         "recipeData": {
@@ -990,7 +990,7 @@ export const foodItems = [
     },
     {
         "id": "94337",
-        "imageUrl": "https://source.unsplash.com/random/1600x900/?food",
+        "imageUrl": "https://cdn.pixabay.com/photo/2021/09/16/16/13/tarte-flambee-6630501_1280.jpg",
         "name": "Corn-On-The-Cob",
         "description": "Spicy Mixed Olives, Red Pepper Dip.",
         "recipeData": {
@@ -1013,7 +1013,7 @@ export const foodItems = [
     },
     {
         "id": "94364",
-        "imageUrl": "https://source.unsplash.com/random/1600x900/?food",
+        "imageUrl": "https://cdn.pixabay.com/photo/2016/03/05/19/02/hamburger-1238246_1280.jpg",
         "name": "Chicken Butterfly",
         "description": "Succulent Double Chicken Breast in Crispy Skin",
         "recipeData": {
